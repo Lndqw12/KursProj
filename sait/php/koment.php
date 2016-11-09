af3d660924f1b139238f48eb491061cffe4de2fc
Lndqw12 <COMP222012016@mail.ru>
2016-11-09 14:27:30 +0300

    KP/8

--- sait/php/koment.php
+++ sait/php/koment.php
@@ -1,23 +1,4 @@
-﻿<meta charset="utf-8">
-<?php
-  /* Принимаем данные из формы */
-  $name = $_POST["name"];
-  $page_id = $_POST["page_id"];
-  $text_comment = $_POST["text_comment"];
-  $name = htmlspecialchars($name);// Преобразуем спецсимволы в HTML-сущности
-  $text_comment = htmlspecialchars($text_comment);// Преобразуем спецсимволы в HTML-сущности
-  $mysqli = new mysqli("localhost", "root", "", "db");// Подключается к базе данных
-  $mysqli->query("INSERT INTO `comments` (`name`, `page_id`, `text_comment`) VALUES ('$name', '$page_id', '$text_comment')");// Добавляем комментарий в таблицу
-  header("Location: ".$_SERVER["HTTP_REFERER"]);// Делаем реридект обратно
-?>
-<!--
-И, наконец, последняя стадия - это вывод комментариев на странице:
-<php
-  $page_id = 150;// Уникальный идентификатор страницы (статьи или поста)
-  $mysqli = new mysqli("localhost", "root", "", "db");// Подключается к базе данных
-  $result_set = $mysqli->query("SELECT * FROM `comments` WHERE `page_id`='$page_id'"); //Вытаскиваем все комментарии для данной страницы
-  while ($row = $result_set->fetch_assoc()) {
-    print_r($row); //Вывод комментариев
-    echo "<br />";
-  }
-?-->
+Content-Type: application/x-www-form-urlencoded
+Content-Length: 30
+
+name=&text_comment=&page_id=50
